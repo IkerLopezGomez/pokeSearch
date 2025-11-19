2cb228568332440b96388e84348242d44bbe56b4
IkerLopez <a24ikelopgom@inspedralbes.cat>
2025-11-19 09:06:16 +0100

    añadido favoritos

--- pokeSearch/src/router/index.js
+++ pokeSearch/src/router/index.js
@@ -1,19 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import buscarPoke from '@/components/buscarPoke.vue';
-import seleccionarPoke from '@/components/seleccionarPokemon.vue';
-import infoPoke from '@/components/infoPokemon.vue';
-import gameEngine from '@/components/gameEngine.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import buscarPoke from "@/components/buscarPoke.vue";
+import seleccionarPoke from "@/components/seleccionarPokemon.vue";
+import infoPoke from "@/components/infoPokemon.vue";
+import gameEngine from "@/components/gameEngine.vue";
+import favoritos from "@/components/favoritos.vue";
 
 const routes = [
-    { path: '/buscar', component: buscarPoke },
-    { path: '/lista', component: seleccionarPoke },
-    { path: '/info', component: infoPoke },
-    { path: '/game', component: gameEngine }
+  { path: "/buscar", component: buscarPoke },
+  { path: "/lista", component: seleccionarPoke },
+  { path: "/info", component: infoPoke },
+  { path: "/game", component: gameEngine },
+  { path: "/favoritos", component: favoritos },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
