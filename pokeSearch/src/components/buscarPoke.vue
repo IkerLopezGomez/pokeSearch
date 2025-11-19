51d3c1ef29a3cf5c0ef351b8a2b398382405f230
IkerLopez <a24ikelopgom@inspedralbes.cat>
2025-11-19 09:52:45 +0100

    ruta puesta correctamente

--- pokeSearch/src/components/buscarPoke.vue
+++ pokeSearch/src/components/buscarPoke.vue
@@ -1,7 +1,7 @@
 <script setup>
     import { ref } from 'vue';
     import axios from 'axios';
-    import { fetchPokemonList } from '../services/communicationManager.js';
+    import { fetchPokemonList } from '../../services/communicationManager.js';
     async function pokemonList() {
         try {
             const data = await fetchPokemonList(50, 0);
